544e03943d2723ff39af8c69c1682e2cbb2bbcec
Ashot <avhmail@gmail.com>
2024-10-10 21:57:57 +0200

    added env variable

--- php-db-conn/np02cachedvals.php
+++ php-db-conn/np02cachedvals.php
@@ -3,14 +3,10 @@ set_time_limit(0);
 ini_set('memory_limit', '6G');
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
-// $api_address = getenv("API_ADDRESS");
-
 
 $elemName = $_GET["elemName"];
-// $url = $api_address."/latest/" . $elemName;
-// $json = json_decode(file_get_contents($url));
-// echo(json_encode($json));
-// echo($api_address);
-echo(json_encode(array("elemname"=>$elemName)));
+$url = "http://188.185.78.106:5000/latest/" . $elemName;
+$json = json_decode(file_get_contents($url));
+    echo(json_encode($json));
 
 ?>
