0672b2fd461a144e5dd8be6e4b4e09ce8638913a
Ashot <avhmail@gmail.com>
2024-10-11 00:07:27 +0200

    added env variable

--- php-db-conn/np02cachedvals.php
+++ php-db-conn/np02cachedvals.php
@@ -10,6 +10,6 @@ $api_address = getenv('API_ADDRESS');
 // $json = json_decode(file_get_contents($url));
 //     echo(json_encode($json));
 // echo($api_address);
-echo $_SERVER['API_ADDRESS'];
+echo $_ENV['API_ADDRESS'];
 
 ?>
